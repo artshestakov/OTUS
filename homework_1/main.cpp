604ac1c901d19d8ce5a369fd538fb6f1ea096746
Shestakov Artem <art.shestakov@icloud.com>
2023-07-05 14:29:51 +0300

    Добавлен вывод текущей конфигурации

--- homework_1/main.cpp
+++ homework_1/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int, char **)
 {
-	std::cout << "Build " << VERSION_REVISION << std::endl;
+	std::cout << "Build " << VERSION_REVISION << ", " << CONFIGURATION << std::endl;
 	std::cout << "Compiled at " << COMPILE_TS_TIME << " on " << COMPILE_TS_DATE << std::endl;
 	std::cout << "Hello world!" << std::endl;
 	return 0;
